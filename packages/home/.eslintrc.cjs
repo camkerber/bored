f6a2c6388c040c6bafe19e6f1b863603dd9976e5
CameronKerber <kerber114@gmail.com>
2024-09-13 14:59:38 -0600

    clean up

--- packages/home/.eslintrc.cjs
+++ packages/home/.eslintrc.cjs
@@ -1,8 +1,32 @@
-const {resolve} = require("path");
-
 module.exports = {
-  extends: "@bored/eslint-config",
+  root: true,
+  env: {browser: true, es2020: true},
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react-hooks/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: resolve(__dirname, "./tsconfig.json"),
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      {allowConstantExport: true},
+    ],
+    "@typescript-eslint/no-misused-promises": [
+      "error",
+      {
+        checksVoidReturn: false,
+      },
+    ],
   },
 };
