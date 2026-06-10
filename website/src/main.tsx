6c6b8cf72cb4c1515090b59986f1e64e4566183e
camkerber <camjkerber@gmail.com>
2026-06-10 12:48:49 -0500

    feat(website): add vercel speed insights

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -5,6 +5,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import {ThemeProvider} from "@mui/material/styles";
 import {SnackbarProvider} from "notistack";
 import {Analytics} from "@vercel/analytics/react";
+import {SpeedInsights} from "@vercel/speed-insights/react";
 import {theme} from "./theme";
 
 createRoot(document.getElementById("root")!).render(
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
         <CssBaseline />
         <AppRouter />
         <Analytics />
+        <SpeedInsights />
       </SnackbarProvider>
     </ThemeProvider>
   </StrictMode>,
